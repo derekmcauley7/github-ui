87ed16d4125b82cf89447505763861af9c0272ab
Derek McAuley <derekmcauley7@gmail.com>
2018-04-15 11:07:46 +0100

    first commit

--- app/router.js
+++ app/router.js
@@ -7,6 +7,14 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('orgs', {});  //  /orgs
+   this.route('org', { path: 'org/:id' }, function() { // org/emberjs
+    this.route('repos', {});
+    this.route('repo', { path: ':repoid'}, function() {
+      this.route('contributors', {});
+      this.route('issues', {});
+    });
+  });
 });
 
 export default Router;
